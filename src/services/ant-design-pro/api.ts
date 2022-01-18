181a25839e31101ad13fba9e28a104ee6455088a
John <john791016@gmail.com>
2022-01-18 16:44:38 +0800

    🐛 fix(compiler): change api

--- src/services/ant-design-pro/api.ts
+++ src/services/ant-design-pro/api.ts
@@ -22,7 +22,7 @@ export async function outLogin(options?: { [key: string]: any }) {
 
 /** 登录接口 POST /api/login/account */
 export async function login(body: API.LoginParams, options?: { [key: string]: any }) {
-  return request<API.LoginResult>('/api/login/account', {
+  return request<API.LoginResult>('https://tempapi.proj.me/api/4DueoP9k4', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
